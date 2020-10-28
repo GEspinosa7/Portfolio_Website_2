71d3d0d74f71ce42e893bf5aab9ea0304c72b842
Gabriel Espinosa <gabrielespinosajob@gmail.com>
2020-10-28 20:43:07 -0300

    create fun functions to buttons

---
+++ js/alerts.js
@@ -0,0 +1,37 @@
+document.getElementById("teste").onclick = alert;
+
+function alert(){
+   document.getElementById("alert").innerHTML = "the buttons of this website are just for view";
+}
+
+function mOverS(obj) {
+   obj.innerHTML = "i'm not gonna do anything"
+}
+
+function mOver(obj) {
+   obj.innerHTML = "i don't do nothing";
+}
+ 
+ function mOut(obj) {
+   obj.innerHTML = "Just View";
+}
+
+function mDown(obj) {
+   obj.innerHTML = "Release me";
+}
+ 
+function mUp(obj) {
+   obj.innerHTML="i've told you before";
+}
+
+function mOverFabOne(obj) {
+   obj.innerHTML="are"
+}
+
+function mOverFabTwo(obj) {
+   obj.innerHTML="you"
+}
+
+function mOverFabThree(obj) {
+   obj.innerHTML="serious?"
+}
